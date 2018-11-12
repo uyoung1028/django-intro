b6deff042a9dbe5b79ee091ce3d090966433a1f8
uyoung <uyoung1028@gmail.com>
2018-11-12 06:38:37 +0000

    django_RUD

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 # 현재 폴더에 있는 views 파일 추가
+app_name = 'todo'
 
 urlpatterns = [
     # 이 아래 들어가는 url들의 앞에 todos/가 생략
     path('', views.index),
     path('new/', views.new),
-    path('create'),
+    path('create/', views.create),
+    path('<int:id>/', views.read),
+    path('todo_create/', views.todo_create),
+    path('<int:id>/update/', views.update, name='update'),
+    path('<int:id>/delete/', views.delete, name='delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todo.models import Todo
 
 # Create your views here.
@@ -8,3 +8,40 @@ def index(request):
     
 def new(request):
     return render(request, 'todo/new.html')
+    
+def create(request):
+    title = request.POST.get('title')
+    deadline = request.POST.get('deadline')
+    todo = Todo(title=title, deadline=deadline)
+    todo.save()
+    return redirect('/todos/')
+
+def read(request, id):
+    todo = Todo.objects.get(id=id)
+    return render(request, 'todo/read.html', {'todo': todo})
+    
+def todo_create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        deadline = request.POST.get('deadline')
+        Todo.objects.create(title=title, deadline=deadline)
+        return redirect('/todos/')
+    else:
+        return render(request, 'todo/todo_create.html')
+        
+def update(request, id):
+    todo = Todo.objects.get(id=id)
+    if request.method == "POST":  # 저장로직
+        todo.title = request.POST.get('title')
+        todo.deadline = request.POST.get('deadline')
+        todo.save()
+        return redirect('/todos/')
+        
+    else:  # 폼 보여주기
+        deadline = todo.deadline.strftime("%Y-%m-%d")
+        return render(request, 'todo/update.html', {'todo': todo, 'deadline': deadline})
+        
+def delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/todos/')
